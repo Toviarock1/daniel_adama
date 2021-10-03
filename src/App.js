1ead43a036664580bb76f6e6fc51b15b7acd88f2
simon adama <simontoviaadama@gmail.com>
2021-10-03 21:14:24 +0100

    restructure layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Layout from "./hoc/Layout/Layout";
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import GetInTouch from './components/GetInTouch/GetInTouch';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout />
+      <Layout>
+        <Hero />
+        <About />
+        <GetInTouch />
+        <Portfolio />
+        <Contact />
+      </Layout>
     </BrowserRouter>
   );
 }
--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import About from '../../components/About/About';
-import Contact from '../../components/Contact/Contact';
-import GetInTouch from '../../components/GetInTouch/GetInTouch';
-import Hero from '../../components/Hero/Hero';
-import Portfolio from '../../components/Portfolio/Portfolio';
 import Header from './../../container/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-const Layout = () => {
+const Layout = (props) => {
     return (
         <>
             <Header />
-            <Hero />
-            <About />
-            <GetInTouch />
-            <Portfolio />
-            <Contact />
+            {props.children}
             <Footer />
         </>
     )
