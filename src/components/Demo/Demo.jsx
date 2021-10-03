efd9696f35aefec5d53e7b0b64a19e7f236c332b
simon adama <simontoviaadama@gmail.com>
2021-10-04 00:23:35 +0100

    add video to demo component

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import { Container } from 'reactstrap'
 import classes from './Demo.module.css'
+import video from './../../assets/video/3. Arrow Functions.mp4';
 
 const Demo = () => {
     return (
         <div className={classes.Demo}>
             <Container>
-                <video>
-                    <source></source>
-                </video>
+                <ReactPlayer url={video} width="100%" height="50%" controls={true} />
             </Container>
         </div>
     )
