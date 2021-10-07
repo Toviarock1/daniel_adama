368832d1a7eaa6dbcc1da9ae1b274f66fe2bc91a
simon adama <simontoviaadama@gmail.com>
2021-10-08 00:24:46 +0100

    add project files

--- src/views/Demo/Demo.jsx
+++ src/views/Demo/Demo.jsx
@@ -3,16 +3,14 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 //reactstrap
 import { Container } from 'reactstrap'
-//video
-import video from './../../assets/video/3. Arrow Functions.mp4';
 //css modules
 import classes from './Demo.module.css'
 
-const Demo = () => {
+const Demo = (props) => {
     return (
         <div className={classes.Demo}>
             <Container className={classes.Container}>
-                <ReactPlayer url={video} width="100%" height="100%" controls={true} />
+                <ReactPlayer url={props.video} width="100%" height="100%" controls={true} />
             </Container>
         </div>
     )
