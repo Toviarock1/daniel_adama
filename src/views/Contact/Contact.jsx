e44683509eeee8981953f8e81559ee5c8ae72000
simon adama <simontoviaadama@gmail.com>
2021-10-22 00:18:06 +0100

    add animation to contact

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -6,6 +6,7 @@ import { ImGithub, ImTwitter, ImLinkedin } from "react-icons/im";
 //animation
 import ScrollAnim, { Parallax } from 'rc-scroll-anim';
 import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
 //component
 import Form from '../../components/Form/Form'
 //css modules
@@ -20,39 +21,51 @@ const Contact = (props) => {
             <Container className={classes.ContactContent}>
                 <Row className="ResponsiveRow">
                     <Col className="ResponsiveMargin">
-                        <Parallax
-                            always={false}
-                            animation={{ x: 0, opacity: 1, playScale: [0.1, 0.9] }}
-                            style={{ transform: 'translateX(-100px)', opacity: 0 }}
-                        >
-                            <p><b>Email:</b> adamadaniel321@gmail.com</p>
-                            <p><b>Phone No:</b> +2348133108645</p>
-                            <Row>
-                                <Col>
-                                    <a href="http://www.linkedin.com/in/danieladama" className={classes.IconWrapper}>
-                                        <ImLinkedin />
-                                    </a>
-                                </Col>
-                                <Col>
-                                    <a href="http://github.com/danielAdama" className={classes.IconWrapper}>
-                                        <ImGithub />
-                                    </a>
-                                </Col>
-                                <Col>
-                                    <a href="https://twitter.com/danieltovia" className={classes.IconWrapper}>
-                                        <ImTwitter />
-                                    </a>
-                                </Col>
-                            </Row>
-                        </Parallax>
-
+                        <ScrollOverPack always={false}>
+                            <TweenOne
+                                animation={{
+                                    x: 0,
+                                    yoyo: false,
+                                    repeat: 0,
+                                    duration: 1000,
+                                    opacity:1
+                                }}
+                                style={{ transform: 'translateX(-100px)', opacity: 0 }}
+                            >
+                                <p><b>Email:</b> adamadaniel321@gmail.com</p>
+                                <p><b>Phone No:</b> +2348133108645</p>
+                                <Row>
+                                    <Col>
+                                        <a href="http://www.linkedin.com/in/danieladama" className={classes.IconWrapper}>
+                                            <ImLinkedin />
+                                        </a>
+                                    </Col>
+                                    <Col>
+                                        <a href="http://github.com/danielAdama" className={classes.IconWrapper}>
+                                            <ImGithub />
+                                        </a>
+                                    </Col>
+                                    <Col>
+                                        <a href="https://twitter.com/danieltovia" className={classes.IconWrapper}>
+                                            <ImTwitter />
+                                        </a>
+                                    </Col>
+                                </Row>
+                            </TweenOne>
+                        </ScrollOverPack>
                     </Col>
                     <Col>
-                        <Parallax
-                            always={false}
-                            animation={{ x: 0, opacity: 1 }}
-                            style={{ transform: 'translateX(100px)', opacity: 0 }}
-                        >
+                    <ScrollOverPack always={false}>
+                            <TweenOne
+                                animation={{
+                                    x: 0,
+                                    yoyo: false,
+                                    repeat: 0,
+                                    duration: 1000,
+                                    opacity:1
+                                }}
+                                style={{ transform: 'translateX(100px)', opacity: 0 }}
+                            >
                             <Form
                                 submit={props.submit}
                                 toggle={props.toggle}
@@ -67,7 +80,8 @@ const Contact = (props) => {
                                 message={props.message}
                                 loading={props.loading}
                             />
-                        </Parallax>
+                            </TweenOne>
+                        </ScrollOverPack>
                     </Col>
                 </Row>
             </Container>
